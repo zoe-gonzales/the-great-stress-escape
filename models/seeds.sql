ff8583bff2af5cc152511b0d620e8a3f78d1497f
Sara Overby <saraoverby@Mendys-MacBook-Air.local>
2019-04-04 20:46:29 -0600

    schema and seeds

---
+++ models/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', landscape);
+INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', plants);
+INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', color);
+INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', abstract);
+INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', landscape);
