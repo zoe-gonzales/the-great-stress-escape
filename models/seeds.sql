9a914726af0ef1fce69af5090d1481369d17804d
Sara Overby <saraoverby@Mendys-MacBook-Air.local>
2019-04-06 11:53:29 -0600

    URL relative paths added to seeds file

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,5 +1,32 @@
-INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', landscape);
-INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', plants);
-INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', color);
-INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', abstract);
-INSERT INTO images (imageurl, category) VALUES ('http://www.google.com/', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/calm-1.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/calm-2.jpg', plants);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/dog-being-taken-by-a-ufo.jpg', animals);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/eagles-nest.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/eagles-nest2.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/flowers.JPG', plants);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/flowers2.JPG', plants);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/great-sand-dunes.jpg', plants);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/lounging.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/nola.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/ocean.jpg', plants);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/reservoir.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/san-fran-sunset.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/shiprock.JPG', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/snowdog.jpg', animals);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/stars.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/sunset.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/sunset12.jpg', landscape);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/trees.jpg', plants);
+INSERT INTO images (imageurl, category) VALUES ('public/assets/images/vines.jpg', plants);
+
+
+
+
+
+INSERT INTO quotes (quote, category) VALUES ('http://www.google.com/', landscape);
+INSERT INTO quotes (quote, category) VALUES ('http://www.google.com/', landscape);
+INSERT INTO quotes (quote, category) VALUES ('http://www.google.com/', landscape);
+INSERT INTO quotes (quote, category) VALUES ('http://www.google.com/', landscape);
+INSERT INTO quotes (quote, category) VALUES ('http://www.google.com/', landscape);
+
+
