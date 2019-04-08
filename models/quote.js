859757bcfdd5e93ea7e4b446160499146ef9a463
Sara Overby <saraoverby@Mendys-MacBook-Air.local>
2019-04-08 15:11:58 -0600

    quote.js edited

--- models/quote.js
+++ models/quote.js
@@ -1,21 +1,28 @@
-module.exports = function(sequelize, DataTypes) {
-    var Quote = sequelize.define("Quote", {
-      quote: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      category: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal('NOW()')
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal('NOW()')
-      }
-    });
-    return Quote;
-  };
+//readme for iquotes: https://github.com/banminkyoz/iquotes
+const iquotes = require('iquotes');
+
+iquotes.random();
+
+//Get random quote for a category
+//Categories: life, love, dev (development), and all
+iquotes.random('life');
+
+
+//function from npm test file
+//https://github.com/banminkyoz/iquotes/blob/master/test.js
+function lifeQuotes(data, category) {
+  const result = data.filter(d => d.category === category);
+  return result ? result.length === data.length : true;
+}
+
+//----> returns error: data.filter is not a function
+
+lifeQuotes('get random', t => {
+  t.truthy(iquotes.random('life'));
+});
+
+lifeQuotes();
+
+module.exports = Search;
+
+
