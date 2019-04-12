a4cdc1b8d6de16e67aca0d72b1896f940a0440d5
Lynn <43070638+LynnAmsbury@users.noreply.github.com>
2019-04-11 19:23:25 -0600

    Adding Jason's image changer code

--- public/js/index.js
+++ public/js/index.js
@@ -116,6 +116,7 @@ $(document).ready(function(){
 // Image Changer
 var imageArray = ["calm_silhouette.jpg", "calm-1.jpg", "calm-2.jpg", "calm-3.jpg", "dog-being-taken-by-a-ufo.jpg" , "eagles-nest.jpg" , "eagles-nest2.jpg" , "flowers.jpg" , "flowers2.jpg" , "great-sand-dunes.jpg" , "inspiring_words.jpg" , "jerusalem-artichoke-blue-sky.jpg" , "lounging.jpg" , "music_notes.jpg" , "nola.jpg" , "ocean.jpg" , "reservoir.jpg" , "san-fran-sunset.jpg" , "shiprock.jpg" , "snowdog.jpg" , "stars.jpg" , "sun_clouds.jpg" , "sunset.jpg" , "sunset11.jpg" , "sunset12.jpg" , "trees.jpg" , "vines.jpg"]
 var calmImage = document.getElementById("calm-image")
+
 function addImage() {
   var randomIndex = Math.floor(Math.random() * 26)
   console.log("randomIndex: ", randomIndex)
